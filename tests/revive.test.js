1b7f6954ed453e05c8d8454f8b0b6aa704798dc7
get <waldemar.reusch@googlemail.com>
2017-08-14 22:56:05 +0200

    refine the revive methods

--- tests/revive.test.js
+++ tests/revive.test.js
@@ -2,6 +2,10 @@ const test = require('tape')
 const fs = require('fs')
 
 const { Document, revive, createReader, createBuilder } = require('..')
+const {
+  Model, ListOfSpecies, ListOfReactions, ListOfCompartments,
+  Compartment, Species, Reaction
+} = Document
 
 test((t) => {
   fs.createReadStream('tests/resources/gly.xml')
@@ -11,11 +15,17 @@ test((t) => {
       t.ok(a instanceof Document)
       const b = JSON.parse(JSON.stringify(a))
       t.not(b instanceof Document)
-      const c = revive(a)
+      const c = revive(b)
       t.ok(c instanceof Document)
-      t.deepEqual(c, a)
-      t.ok(c.model instanceof Document.Model)
-      console.log(JSON.stringify(a, null, 2))
+      t.ok(c.model instanceof Model)
+      t.ok(c.model.listOfSpecies instanceof ListOfSpecies)
+      t.ok(c.model.listOfReactions instanceof ListOfReactions)
+      t.ok(c.model.listOfCompartments instanceof ListOfCompartments)
+
+      for (let x of c.model.listOfSpecies) t.ok(x instanceof Species)
+      for (let x of c.model.listOfReactions) t.ok(x instanceof Reaction)
+      for (let x of c.model.listOfCompartments) t.ok(x instanceof Compartment)
+
       t.end()
     })
     .on('error', (e) => t.fail(e.message))
