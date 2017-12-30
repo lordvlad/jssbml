1750d3d37dcbd1edc47ba7b9de796e19bbf541dd
Waldemar Reusch <waldemar.reusch@googlemail.com>
2017-12-30 14:07:57 +0100

    enable all tests

--- tests/reader.test.js
+++ tests/reader.test.js
@@ -3,7 +3,7 @@ const {testReader, testRevive, testRoundTrip} = require('./util')
 
 test('parse tca', (t) => testReader(t, 'tests/resources/tca.xml', 2, 26, 17))
 test('parse gly', (t) => testReader(t, 'tests/resources/gly.xml', 2, 25, 19, true, true))
-test.only('roundtrip tca', (t) => testRoundTrip('tests/resources/tca.xml', t))
+test('roundtrip tca', (t) => testRoundTrip('tests/resources/tca.xml', t))
 test('roundtrip gly', (t) => testRoundTrip('tests/resources/gly.xml', t))
 test('revive tca', (t) => testRevive('tests/resources/tca.xml', t, 2, 26, 17))
 test('revive gly', (t) => testRevive('tests/resources/gly.xml', t, 2, 25, 19, true, true))
